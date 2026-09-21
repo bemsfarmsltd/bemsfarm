aa6630fda5dd4e3c1237ff0b283fe614af32381b
Henry Adeleke <henry@Henrys-MacBook-Pro.local>
2026-09-21 13:00:49 +0100

    chore: clean up obsolete scratch scripts and temporary root artifacts

--- server/test_tables.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config({path: './.env'});
-const { Pool } = require('pg');
-const pool = new Pool({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } });
-pool.query("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'").then(res => {
-  console.log(res.rows.map(r => r.table_name).join(', '));
-  pool.end();
-}).catch(console.error);
